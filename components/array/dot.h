7d1e666614a7a68b849faaf85c5d64443ec88e22
Justin Ugwudike <jknu2001@gmail.com>
2025-01-11 17:55:26 +0000

    fixed dot products

--- components/array/dot.h
+++ components/array/dot.h
@@ -59,23 +59,23 @@ Array<D> Array<D>::try_dot_arr1(Array<D> &rhs)
 
         break;
     case 4:
-        // arr1.arr4
-        arrays = rhs.split(0);
-        dim1 = rhs.shape.shape()[1];
-        dim2 = rhs.shape.shape()[3];
+        // arr1.arr4 OK
+        _rhs = rhs.permute({0, 2, 1, 3});
+        arrays = _rhs.split(0);
+        dim1 = _rhs.shape.shape()[2];
+        dim2 = _rhs.shape.shape()[3];
 
         for (int i = 0; i < rhs.shape.shape()[0]; i++)
         {
             _rhs = arrays[i];
-            _rhs = _rhs.reshape({arrays[0].shape.shape()[2], dim1 * dim2});
+            _rhs = _rhs.reshape({arrays[0].shape.shape()[0], dim1 * dim2});
 
             result = this->try_dot(*this, _rhs);
-            result = result.reshape({this->shape.shape()[1], dim1, dim2});
-            result_arrays.push_back(result);
+            result = result.reshape({this->shape.shape()[0], dim1, dim2});
+            result_arrays.push_back(result.permute({1,0,2}));
         }
 
         result = stack(result_arrays, 0);
-        result = result.squeeze(3);
         break;
     default:
         std::cout << "Cannot compute dot product between array" << this->rank << " and array" << rhs.rank << std::endl;
@@ -115,22 +115,23 @@ Array<D> Array<D>::try_dot_arr2(Array<D> &rhs)
         result = result.permute({1,0,2});
         break;
     case 4:
-        // arr2.arr4
-        arrays = rhs.split(3);
-        dim1 = rhs.shape.shape()[1];
-        dim2 = rhs.shape.shape()[2];
+        // arr2.arr4 OK
+        _rhs = rhs.permute({0, 2, 1, 3});
+        arrays = _rhs.split(0);
+        dim1 = _rhs.shape.shape()[2];
+        dim2 = _rhs.shape.shape()[3];
 
-        for (int i = 0; i < rhs.shape.shape()[3]; i++)
+        for (int i = 0; i < rhs.shape.shape()[0]; i++)
         {
             _rhs = arrays[i];
             _rhs = _rhs.reshape({arrays[0].shape.shape()[0], dim1 * dim2});
 
             result = this->try_dot(*this, _rhs);
             result = result.reshape({this->shape.shape()[0], dim1, dim2});
-            result_arrays.push_back(result);
+            result_arrays.push_back(result.permute({1,0,2}));
         }
 
-        result = stack(result_arrays, 3);
+        result = stack(result_arrays, 0);
         break;
     default:
         std::cout << "Cannot compute dot product between array" << this->rank << " and array" << rhs.rank << std::endl;
@@ -143,8 +144,9 @@ Array<D> Array<D>::try_dot_arr2(Array<D> &rhs)
 template <typename D>
 Array<D> Array<D>::try_dot_arr3(Array<D> &rhs)
 {
-    Array<D> lhs, _rhs, result, result_arrays;
-    int dim1, dim2, dim3, dim4;
+    Array<D> lhs, _rhs, result;
+    int dim1, dim2, length, length1, lhs_incr = 1, rhs_incr = 1;
+    std::vector<Array<D>> lhs_arrays, lhs_arrays1, rhs_arrays, rhs_arrays1, result_arrays;
 
     switch (rhs.rank)
     {
@@ -171,24 +173,95 @@ Array<D> Array<D>::try_dot_arr3(Array<D> &rhs)
         result = result.reshape({dim1, dim2, rhs.shape.shape()[1]});
         break;
     case 3:
-        // arr3.arr3
+        // arr3.arr3 OK
+
         lhs = *this;
 
-        _rhs = rhs.permute({1,0,2});
-        dim1 = _rhs.shape.shape()[1];
-        dim2 = _rhs.shape.shape()[2];
-
-        for(int i = 0; i < lhs.shape.shape()[0]; i++){
-
-            lhs = lhs.reshape({dim1 * dim2, lhs.shape.shape()[2]});
-            _rhs = _rhs.reshape({rhs.shape.shape()[0], dim3 * dim4});
-
-            result_arrays.push_back(this->try_dot(lhs, _rhs));
+        if(rhs.shape.shape()[0] > lhs.shape.shape()[0]){
+            if(rhs.shape.shape()[0] % lhs.shape.shape()[0] != 0){
+                std::cout << "Cannot compute dot product between arrays with these shapes";
+                exit(0);
+            }
+            else{
+                length = rhs.shape.shape()[0];
+                rhs_incr = length;
+                lhs_incr = lhs.shape.shape()[0];
+            }
+        }else{
+            if(lhs.shape.shape()[0] % rhs.shape.shape()[0] != 0){
+                std::cout << "Cannot compute dot product between arrays with these shapes";
+                exit(0);
+            }
+            else{
+                length = lhs.shape.shape()[0];
+                lhs_incr = length;
+                rhs_incr = rhs.shape.shape()[0];
+            }
         }
 
-        result = stack(result_arrays, 0)
+        lhs_arrays = lhs.split(0);
+        rhs_arrays = rhs.split(0);
 
-        result = result.reshape({rhs, dim2, dim3, dim4});
+        for(int i = 0; i < length; i++){
+
+            lhs = lhs_arrays[i % lhs_incr];
+            _rhs = rhs_arrays[i % rhs_incr];
+
+            result = this->try_dot(lhs, _rhs);
+            result_arrays.push_back(result);
+        }
+
+        result = stack(result_arrays, 0);
+        break;
+    case 4:
+        // arr3.arr4 
+
+        lhs = *this;
+
+        if(rhs.shape.shape()[1] > lhs.shape.shape()[0]){
+            if(rhs.shape.shape()[1] % lhs.shape.shape()[0] != 0){
+                std::cout << "Cannot compute dot product between arrays with these shapes";
+                exit(0);
+            }
+            else{
+                length = rhs.shape.shape()[1];
+                rhs_incr = length;
+                lhs_incr = lhs.shape.shape()[0];
+            }
+        }else{
+            if(lhs.shape.shape()[0] % rhs.shape.shape()[1] != 0){
+                std::cout << "Cannot compute dot product between arrays with these shapes";
+                exit(0);
+            }
+            else{
+                length = lhs.shape.shape()[0];
+                lhs_incr = length;
+                rhs_incr = rhs.shape.shape()[1];
+            }
+        }
+
+        lhs_arrays = lhs.split(0);
+        rhs_arrays = rhs.split(0);
+        length1 = rhs.shape.shape()[0];
+
+        for(int i = 0; i < length1; i++){
+
+            rhs_arrays1 = rhs_arrays[i].split(0);
+            std::vector<Array<D>> result_arrays1;
+
+            for(int j = 0; j < length; j++){
+
+                lhs = lhs_arrays[j % lhs_incr];
+                _rhs = rhs_arrays1[j % rhs_incr];
+
+                result = this->try_dot(lhs, _rhs);
+                result_arrays1.push_back(result);
+            }
+            result = stack(result_arrays1, 0);
+            result_arrays.push_back(result);
+        }
+
+        result = stack(result_arrays, 0);
         break;
     default:
         std::cout << "Cannot compute dot product between array" << this->rank << " and array" << rhs.rank << std::endl;
@@ -202,83 +275,160 @@ template <typename D>
 Array<D> Array<D>::try_dot_arr4(Array<D> &rhs)
 {
     Array<D> lhs, _rhs, result;
-    int dim1, dim2, dim3, dim4;
-    std::vector<Array<D>> arrays, arrays1, arrays2, arrays3, result_arrays;
+    int dim1, dim2, length, length1, lhs_incr = 1, rhs_incr = 1, lhs_incr1 = 1, rhs_incr1 = 1;
+    std::vector<Array<D>> lhs_arrays, lhs_arrays1, rhs_arrays, rhs_arrays1, result_arrays;
 
     switch (rhs.rank)
     {
     case 1:
-        // arr4.arr1
-        arrays = this->split(0);
+        // arr4.arr1 OK
+        lhs_arrays = this->split(0);
         dim1 = this->shape.shape()[1];
         dim2 = this->shape.shape()[2];
 
         for (int i = 0; i < this->shape.shape()[0]; i++)
         {
-            lhs = arrays[i];
-            lhs = lhs.reshape({dim1 * dim2, arrays[0].get_shape().shape()[2]});
+            lhs = lhs_arrays[i];
+            lhs = lhs.reshape({dim1 * dim2, lhs_arrays[0].shape.shape()[2]});
 
             result = this->try_dot(lhs, rhs);
-            result = result.reshape({dim1, dim2, rhs.get_shape().shape()[1]});
+            result = result.reshape({dim1, dim2, rhs.shape.shape()[1]});
             result_arrays.push_back(result);
         }
         result = stack(result_arrays, 0);
-        result = result.squeeze(3);
         break;
     case 2:
-        // arr4.arr2
-        arrays = this->split(0);
+        // arr4.arr2 OK
+        lhs_arrays = this->split(0);
         dim1 = this->shape.shape()[1];
         dim2 = this->shape.shape()[2];
 
         for (int i = 0; i < this->shape.shape()[0]; i++)
         {
-            lhs = arrays[i];
-            lhs = lhs.reshape({dim1 * dim2, arrays[0].get_shape().shape()[2]});
+            lhs = lhs_arrays[i];
+            lhs = lhs.reshape({dim1 * dim2, lhs_arrays[0].shape.shape()[2]});
 
             result = this->try_dot(lhs, rhs);
-            result = result.reshape({dim1, dim2, rhs.get_shape().shape()[1]});
+            result = result.reshape({dim1, dim2, rhs.shape.shape()[1]});
             result_arrays.push_back(result);
         }
         result = stack(result_arrays, 0);
         break;
-    case 4:
-        // arr4.arr4
-        arrays = this->split(0);
-        arrays1 = rhs.split(0);
-        dim1 = this->shape.shape()[1];
-        dim2 = this->shape.shape()[2];
-        dim3 = rhs.shape.shape()[1];
-        dim4 = rhs.shape.shape()[3];
-                std::cout<<"heyho"<<std::endl;
+    case 3:
+        // arr4.arr3 OK
+        lhs = *this;
 
-        for (int i = 0; i < this->shape.shape()[0]; i++) {
-            arrays2 = arrays[i].split(0);
-            arrays3 = arrays1[i].split(0);
+        if(rhs.shape.shape()[0] > lhs.shape.shape()[1]){
+            if(rhs.shape.shape()[0] % lhs.shape.shape()[1] != 0){
+                std::cout << "Cannot compute dot product between arrays with these shapes";
+                exit(0);
+            }
+            else{
+                length = rhs.shape.shape()[0];
+                rhs_incr = length;
+                lhs_incr = lhs.shape.shape()[1];
+            }
+        }else{
+            if(lhs.shape.shape()[1] % rhs.shape.shape()[0] != 0){
+                std::cout << "Cannot compute dot product between arrays with these shapes";
+                exit(0);
+            }
+            else{
+                length = lhs.shape.shape()[1];
+                lhs_incr = length;
+                rhs_incr = rhs.shape.shape()[0];
+            }
+        }
+
+        lhs_arrays = lhs.split(0);
+        rhs_arrays = rhs.split(0);
+
+        for(int i = 0; i< this->shape.shape()[0]; i++){
+
+            lhs_arrays1 = lhs_arrays[i].split(0);
             std::vector<Array<D>> result_arrays1;
-            for (int j = 0; j < this->shape.shape()[1]; j++) {
-                lhs = arrays2[j];
-                _rhs = arrays3[j];
 
-                lhs.shape.print();
-                _rhs.shape.print();
-                std::cout<<"heyho0"<<std::endl;
+            for(int j = 0; j < length; j++){
+
+                lhs = lhs_arrays1[j % lhs_incr];
+                _rhs = rhs_arrays[j % rhs_incr];
 
                 result = this->try_dot(lhs, _rhs);
-                std::cout<<rhs.get_shape().shape()[3]<<std::endl;
-
-                //result.shape.print();
-                std::cout<<"heyho2"<<std::endl;
-
                 result_arrays1.push_back(result);
             }
             result = stack(result_arrays1, 0);
             result_arrays.push_back(result);
         }
-        std::cout<<"heyho3"<<std::endl;
-        result = stack(result_arrays, 0);
-        std::cout<<"heyho4"<<std::endl;
 
+        result = stack(result_arrays, 0);
+        break;
+    case 4:
+        // arr4.arr4
+        lhs = *this;
+
+        lhs_incr = lhs.shape.shape()[0];
+        rhs_incr = rhs.shape.shape()[0];
+        rhs_incr1 = rhs.shape.shape()[1];
+        lhs_incr1 = lhs.shape.shape()[1];
+
+        if(rhs.shape.shape()[0] > lhs.shape.shape()[0]){
+            if(rhs.shape.shape()[0] % lhs.shape.shape()[0] != 0){
+                std::cout << "Cannot compute dot product between arrays with these shapes";
+                exit(0);
+            }
+            else{
+                length = rhs.shape.shape()[0];
+            }
+        }else{
+            if(lhs.shape.shape()[0] % rhs.shape.shape()[0] != 0){
+                std::cout << "Cannot compute dot product between arrays with these shapes";
+                exit(0);
+            }
+            else{
+                length = lhs.shape.shape()[0];
+            }
+        }
+
+        if(rhs.shape.shape()[1] > lhs.shape.shape()[1]){
+            if(rhs.shape.shape()[1] % lhs.shape.shape()[1] != 0){
+                std::cout << "Cannot compute dot product between arrays with these shapes";
+                exit(0);
+            }
+            else{
+                length1 = rhs.shape.shape()[1];
+            }
+        }else{
+            if(lhs.shape.shape()[1] % rhs.shape.shape()[1] != 0){
+                std::cout << "Cannot compute dot product between arrays with these shapes";
+                exit(0);
+            }
+            else{
+                length1 = lhs.shape.shape()[1];
+            }
+        }
+
+        lhs_arrays = lhs.split(0);
+        rhs_arrays = rhs.split(0);
+
+        for(int i = 0; i< length; i++){
+
+            lhs_arrays1 = lhs_arrays[i % lhs_incr].split(0);
+            rhs_arrays1 = rhs_arrays[i % rhs_incr].split(0);
+            std::vector<Array<D>> result_arrays1;
+
+            for(int j = 0; j < length1; j++){
+
+                lhs = lhs_arrays1[j % lhs_incr1];
+                _rhs = rhs_arrays1[j % rhs_incr1];
+
+                result = this->try_dot(lhs, _rhs);
+                result_arrays1.push_back(result);
+            }
+            result = stack(result_arrays1, 0);
+            result_arrays.push_back(result);
+        }
+
+        result = stack(result_arrays, 0);
         break;
     default:
         std::cout << "Cannot compute dot product between array" << this->rank << " and array" << rhs.rank << std::endl;
--- components/array/tests/test_dot.cpp
+++ components/array/tests/test_dot.cpp
@@ -27,15 +27,35 @@ int test_dot(){
     array2t.get_shape().print();
 
     std::cout<<std::endl<<"ARRAY3::::::::::: "<<std::endl;
-    Array<float> array3({3, 4, 5});
+    Array<float> array3({6, 4, 5});
     array3.set_name("arr3");
     array3.lin();
     array3.get_shape().print();
 
-    Array<float> array3t({3, 5, 4});
+    Array<float> array3t({6, 5, 4});
     array3t.set_name("arr3.t()");
     array3t = array3.t();
     array3t.get_shape().print();
+
+    Array<float> array31({1, 5, 4});
+    array31.set_name("arr31");
+    array31.lin();
+    array31.get_shape().print();
+
+    Array<float> array32({2, 5, 4});
+    array32.set_name("arr32");
+    array32.lin();
+    array32.get_shape().print();
+
+    Array<float> array33({3, 5, 4});
+    array33.set_name("arr33");
+    array33.lin();
+    array33.get_shape().print();
+
+    Array<float> array34({4, 5, 4});
+    array34.set_name("arr34");
+    array34.lin();
+    array34.get_shape().print();
 
     std::cout<<std::endl<<"ARRAY4::::::::::: "<<std::endl;
     Array<float> array4({2, 3, 4, 5});
@@ -47,6 +67,21 @@ int test_dot(){
     array4t.set_name("arr4.t()");
     array4t = array4.t();
     array4t.get_shape().print();
+
+    Array<float> array41({1, 3, 5, 4});
+    array41.set_name("arr41");
+    array41.lin();
+    array41.get_shape().print();
+
+    Array<float> array46({6, 3, 5, 4});
+    array46.set_name("arr46");
+    array46.lin();
+    array46.get_shape().print();
+
+    Array<float> array43({3, 3, 5, 4});
+    array43.set_name("arr43");
+    array43.lin();
+    array43.get_shape().print();
 
     // std::cout<<"------------------------------------------"<<std::endl;
     // std::cout<<std::endl<<"arr1.arr1.t()"<<std::endl;
@@ -74,7 +109,7 @@ int test_dot(){
     // result = array2t.dot(array2);
     // result.print();
 
-    std::cout<<"------------------------------------------"<<std::endl;
+    // std::cout<<"------------------------------------------"<<std::endl;
     // std::cout<<std::endl<<"arr3.arr1.t()"<<std::endl;
     // result = array3.dot(array1t);
     // result.get_shape().print();
@@ -91,13 +126,82 @@ int test_dot(){
     // result = array2t.dot(array3);
     // result.get_shape().print();
 
-    std::cout<<std::endl<<"arr3.arr3.t()"<<std::endl;
-    result = array3.dot(array3t);
+    // std::cout<<std::endl<<"arr3.arr3.t()"<<std::endl;
+    // result = array3.dot(array3t);
+    // result.get_shape().print();
+
+    // std::cout<<std::endl<<"arr3.t().arr3"<<std::endl;
+    // result = array3t.dot(array3);
+    // result.get_shape().print();
+
+    // std::cout<<std::endl<<"arr3.arr31"<<std::endl;
+    // result = array3.dot(array31);
+    // result.get_shape().print();
+
+    // std::cout<<std::endl<<"arr3.arr32"<<std::endl;
+    // result = array3.dot(array32);
+    // result.get_shape().print();
+
+    // std::cout<<std::endl<<"arr3.arr33"<<std::endl;
+    // result = array3.dot(array33);
+    // result.get_shape().print();
+
+    // std::cout<<std::endl<<"arr3.arr34"<<std::endl; // won't compile
+    // result = array3.dot(array34);
+    // result.get_shape().print();
+
+    std::cout<<"------------------------------------------"<<std::endl;
+    std::cout<<std::endl<<"arr4.arr1.t()"<<std::endl;
+    result = array4.dot(array1t);
     result.get_shape().print();
 
-    std::cout<<std::endl<<"arr3.t().arr3"<<std::endl;
-    result = array3t.dot(array3);
+    std::cout<<std::endl<<"arr1.arr4.t()"<<std::endl;
+    result = array1.dot(array4t);
     result.get_shape().print();
+
+    std::cout<<std::endl<<"arr4.arr2.t()"<<std::endl;
+    result = array4.dot(array2t);
+    result.get_shape().print();
+
+    std::cout<<std::endl<<"arr2.t().arr4"<<std::endl;
+    result = array2t.dot(array4);
+    result.get_shape().print();
+
+    std::cout<<std::endl<<"arr4.arr3.t()"<<std::endl;
+    result = array4.dot(array3t);
+    result.get_shape().print();
+
+    std::cout<<std::endl<<"arr3.t().arr4"<<std::endl;
+    result = array3t.dot(array4);
+    result.get_shape().print();
+
+    std::cout<<std::endl<<"arr4.arr34"<<std::endl;
+    result = array4.dot(array33);
+    result.get_shape().print();
+
+    std::cout<<std::endl<<"arr4.arr31"<<std::endl;
+    result = array4.dot(array31);
+    result.get_shape().print();
+
+    std::cout<<std::endl<<"arr4.arr4.t()"<<std::endl;
+    result = array4.dot(array4t);
+    result.get_shape().print();
+
+    std::cout<<std::endl<<"arr4.t().arr4"<<std::endl;
+    result = array4t.dot(array4);
+    result.get_shape().print();
+
+    std::cout<<std::endl<<"arr4.arr41"<<std::endl;
+    result = array4.dot(array41);
+    result.get_shape().print();
+
+    std::cout<<std::endl<<"arr4.arr46"<<std::endl;
+    result = array4.dot(array46);
+    result.get_shape().print();
+
+    // std::cout<<std::endl<<"arr4.arr43"<<std::endl; // won't compile
+    // result = array4.dot(array43);
+    // result.print();
 
     return 0;
 }
